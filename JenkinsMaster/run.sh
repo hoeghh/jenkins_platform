d0439a817d48c72e3d09984e5cd48d01152d4ea7
hoeghh <henrik@softica.dk>
2015-05-06 10:19:50 +0200

    Removed the name. need to find out how to work around this

--- JenkinsMaster/run.sh
+++ JenkinsMaster/run.sh
@@ -1,1 +1,1 @@
-docker run -d -name jenkins-master -p 8080:8080 -v /data/jenkins:/var/jenkins_home hoeghh/jenkins-master
+docker run -d -p 8080:8080 -v /data/jenkins:/var/jenkins_home hoeghh/jenkins-master
