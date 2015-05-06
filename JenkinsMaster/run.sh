f4db5c1908121a8fabe8e53f74d3a37051073171
hoeghh <henrik@softica.dk>
2015-05-06 12:32:32 +0200

    Now displays ID and IP of Jenkins Master container

--- JenkinsMaster/run.sh
+++ JenkinsMaster/run.sh
@@ -1,1 +1,4 @@
-docker run -d -p 8080:8080 -v /data/jenkins:/var/jenkins_home hoeghh/jenkins-master
+JenkinsMasterId=$(docker run -d -p 8080:8080 -v /data/jenkins:/var/jenkins_home hoeghh/jenkins-master)
+echo "Jenkins master ID: " $JenkinsMasterId
+JenkinsMasterIp=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' $JenkinsMasterId)
+echo "Jenkins master IP: " $JenkinsMasterIp
