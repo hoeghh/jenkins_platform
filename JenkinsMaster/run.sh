4886c4b94327ff13c0471c8170d1c3016c6041de
hoeghh <henrik@softica.dk>
2015-05-06 10:19:04 +0200

    Added -d to run as daemon

--- JenkinsMaster/run.sh
+++ JenkinsMaster/run.sh
@@ -1,1 +1,1 @@
-docker run -name jenkins-master -p 8080:8080 -v /data/jenkins:/var/jenkins_home hoeghh/jenkins-master
+docker run -d -name jenkins-master -p 8080:8080 -v /data/jenkins:/var/jenkins_home hoeghh/jenkins-master
