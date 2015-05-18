3ffefc90412497da92ed9e657c90fb334928628f
Fedora Cloud User <fedora@praqmajenkins.c.no-osl1.zetta.io>
2015-05-18 10:54:48 +0000

    added port 50000 to run command. Its used by tcp slaves

--- JenkinsMaster/run.sh
+++ JenkinsMaster/run.sh
@@ -1,4 +1,4 @@
-JenkinsMasterId=$(docker run -d -p 8080:8080 -v /data/jenkins:/var/jenkins_home hoeghh/jenkins-master)
+JenkinsMasterId=$(docker run -d -p 50000:50000 -p 8080:8080 -v /data/jenkins:/var/jenkins_home hoeghh/jenkins-master)
 echo "Jenkins master ID: " $JenkinsMasterId
 echo "JM_ID="$JenkinsMasterId > jmaster.properties
 JenkinsMasterIp=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' $JenkinsMasterId)
