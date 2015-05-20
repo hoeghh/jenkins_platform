2daf6fb4547b6843eaec493bfba4ab0d377f29ff
hoeghh <heh@praqma.net>
2015-05-20 12:27:31 +0200

    Added comment on how to use a volume

--- Machine/startRegistry.sh
+++ Machine/startRegistry.sh
@@ -2,5 +2,7 @@
 # Check if theres a registry on the host
 # If so, delete it with :
 # docker rm praqmaRegistry
-
+#
+# docker run --name "praqmaRegistry" -v /mnt/praqmaRegistry:/tmp/registry-dev -d -p 5000:5000 registry:2.0
+#
 docker run --name "praqmaRegistry" -d -p 5000:5000 registry:2.0
