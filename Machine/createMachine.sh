7d5770957af789bbbfdc43dc5680308b5ce166bd
hoeghh <heh@praqma.net>
2015-05-20 08:36:10 +0200

    Changed image from CoreOS to Ubuntu

--- Machine/createMachine.sh
+++ Machine/createMachine.sh
@@ -1,9 +1,9 @@
 ./zetta-docker-machine create  \
   --driver openstack  \
   --openstack-flavor-id 6  \
-  --openstack-image-id 5178239d-6853-4da9-8934-53990db845ff  \
-  --openstack-floatingip-pool Public  \
-  --openstack-ssh-user core  \
-  --openstack-net-id be63ef5c-0656-42e5-9f80-8754ff9fcb23 \
+  --openstack-image-id d0a89aa8-9644-408d-a023-4dcc1148ca01  \
+  --openstack-ssh-user ubuntu  \
   --openstack-sec-groups default,DockerAPI  \
-praqmatest2
+  --openstack-floatingip-pool Public  \
+  --openstack-net-id be63ef5c-0656-42e5-9f80-8754ff9fcb23 \
+praqmatest3
