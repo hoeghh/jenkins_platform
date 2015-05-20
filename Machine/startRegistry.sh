c76c5cade9daab5976d04402d27ec7c7d1fbdcc4
hoeghh <heh@praqma.net>
2015-05-20 15:36:03 +0200

    Changed version 2 to 1

--- Machine/startRegistry.sh
+++ Machine/startRegistry.sh
@@ -3,6 +3,6 @@
 # If so, delete it with :
 # docker rm praqmaRegistry
 #
-# docker run --name "praqmaRegistry" -v /mnt/praqmaRegistry:/tmp/registry-dev -d -p 5000:5000 registry:2.0
+# docker run --name "praqmaRegistry" -v /mnt/praqmaRegistry:/tmp/registry-dev -d -p 5000:5000 registry:0.9.1
 #
 docker run --name "praqmaRegistry" -d -p 5000:5000 registry:2.0
