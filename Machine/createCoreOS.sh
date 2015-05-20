8166c5cc162a77451178bde3e07958a52df39272
hoeghh <heh@praqma.net>
2015-05-20 11:31:10 +0200

    added Debug mode

--- Machine/createCoreOS.sh
+++ Machine/createCoreOS.sh
@@ -1,4 +1,4 @@
-./zetta-docker-machine create  \
+./zetta-docker-machine -D create  \
   --driver openstack  \
   --openstack-flavor-id 6  \
   --openstack-image-id 5178239d-6853-4da9-8934-53990db845ff  \
