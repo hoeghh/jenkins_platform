e5d39bab4b71d36d8951824744e549cc426c627d
hoeghh <heh@praqma.net>
2015-05-20 11:48:13 +0200

    Added comment on what to do next time

--- Machine/startRegistry.sh
+++ Machine/startRegistry.sh
@@ -1,1 +1,6 @@
-docker run -d -p 5000:5000 registry:2.0
+# TODO
+# Check if theres a registry on the host
+# If so, delete it with :
+# docker rm praqmaRegistry
+
+docker run --name "praqmaRegistry" -d -p 5000:5000 registry:2.0
